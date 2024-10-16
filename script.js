10ebe6a4d4d9bd93acafb5977909a4d9ac8f8c3d
IanCist <iantcisternas@gmail.com>
2024-10-16 11:55:40 -0400

    added images trying to style description

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ $(document).ready(function () {
     $('#more-info-btn').on('click', function () {
         $('#more-info').toggle();
     });
+
+  
+    $('.trunk-item').hover (
+        function () {
+            $(this).find('.trunk-description').stop(true, true).slideDown();
+        },
+        function () {
+            $(this).find('.trunk-description').stop(true, true).slideUp();
+        }
+    );
 });
+
